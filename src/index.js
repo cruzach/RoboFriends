c5e283ea93b9d9b4ac30a20a9c593d4065ca2bac
cruzach <charliecruzan@gmail.com>
2018-09-26 17:29:54 -0400

    attempt to host on github pages

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import {robots} from './robots.js';
+
+ReactDOM.render(
+    <App robots = {robots}/>
+, document.getElementById('root'));
+registerServiceWorker();
